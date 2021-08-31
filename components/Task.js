356debd26dfe59694eba01d9bb301dbcee908431
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-31 20:39:55 +0800

    Taskjs-start

---
+++ components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Task = () => {
+    return (
+        <View>
+            <Text>This is a task</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default Task;
