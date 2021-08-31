6e43b4e194d53eebb3894435b0c7083b20ea15e8
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-31 20:46:45 +0800

    Appjs-u1

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
@@ -11,6 +11,8 @@ export default function App() {
 
         <View style={styles.items}>
           {/* Tasks items */}
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
         </View>
       </View>
     </View>
